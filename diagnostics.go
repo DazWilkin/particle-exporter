bee05b62ab479070b76706b3416dd746723f34cf
Daz Wilkin <daz.wilkin@gmail.com>
2019-06-28 20:10:52 -0700

    Correction

--- diagnostics.go
+++ diagnostics.go
@@ -45,7 +45,7 @@ func newDiagnostics(token, device string) (response Response, err error) {
 }
 func (d Diagnostic) expose() (result string) {
 	log.Println("[diagnostics:expose] Entered")
-	result += fmt.Sprintf("# HELP device system memory used.\n# TYPE device_system_memory_used gauge\npdevice_system_memory_user{core_id=\"%s\"} %d\n", d.ID, d.Payload.Device.System.Memory.Used)
+	result += fmt.Sprintf("# HELP device system memory used.\n# TYPE device_system_memory_used gauge\ndevice_system_memory_user{core_id=\"%s\"} %d\n", d.ID, d.Payload.Device.System.Memory.Used)
 	result += fmt.Sprintf("# HELP device system memory total.\n# TYPE device_system_memory_total gauge\ndevice_system_memory_total{core_id=\"%s\"} %d\n", d.ID, d.Payload.Device.System.Memory.Total)
 	result += fmt.Sprintf("# HELP device uptime.\n#TYPE device_uptime gauge\ndevice_uptime{core_id=\"%s\"} %d\n", d.ID, d.Payload.Device.System.Uptime)
 	return result
